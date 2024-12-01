d23327ed9ddd6f16941fa11fe7a646ac48a8f279
AllxRise <dahilege@gmail.com>
2024-12-01 13:00:26 +0300

    Day 1 Commit

---
+++ d1/src/main.rs
@@ -0,0 +1,54 @@
+use std::{fs::File, io::read_to_string};
+
+fn main() {
+  let input = read_to_string(File::open("input.txt").unwrap()).unwrap();
+
+  let mut left_list = Vec::with_capacity(1000);
+  let mut right_list = Vec::with_capacity(1000);
+
+  input.lines().for_each(|line| {
+    let mut split = line.split("   ");
+
+    let l: i32 = split.next().unwrap().parse().unwrap();
+    left_list.push(l);
+
+    let r: i32 = split.next().unwrap().parse().unwrap();
+    right_list.push(r);
+  });
+
+  part_one(left_list.clone(), right_list.clone());
+
+  part_two(left_list.clone(), right_list.clone());
+}
+
+fn part_one(mut left_list: Vec<i32>, mut right_list: Vec<i32>) {
+  let mut total_distance = 0;
+
+  left_list.sort();
+  right_list.sort();
+
+  (0..1000).for_each(|i| {
+    let distance = (left_list[i] - right_list[i]).abs();
+    total_distance += distance;
+  });
+
+  println!("Total Distance: {total_distance}");
+}
+
+fn part_two(left_list: Vec<i32>, right_list: Vec<i32>) {
+  let mut total_similarity = 0;
+
+  left_list.iter().for_each(|l| {
+    let mut times = 0;
+
+    right_list.iter().for_each(|r| {
+      if *l == *r {
+        times += 1;
+      }
+    });
+
+    total_similarity += l * times;
+  });
+
+  println!("Total Similarity: {total_similarity}");
+}
